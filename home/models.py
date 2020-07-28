5c45e2263b25e9f87c8204e71bbf6ce712174d20
Lektor <denislour@gmail.com>
2020-07-28 12:20:22 -0400

    [#3] - Work around django relation field and wagtial ImageChoosePanel and PageChoosePanel.

--- home/models.py
+++ home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
@@ -16,9 +18,29 @@ class HomePage(Page):
 
     # Define django fields
     banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
     # Add django fields to content panels in Admin
-    content_panels = Page.content_panels + [FieldPanel("banner_title")]
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
+    ]
 
     class Meta:
         verbose_name = "Home Page"
